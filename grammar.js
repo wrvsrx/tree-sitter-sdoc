de73ee25f4d500170c5e3f0f9b652f5251b874c9
wrvsrx <wrvsrx@outlook.com>
2024-07-28 20:44:13 +0800

    feat: support explicit paragraph

--- grammar.js
+++ grammar.js
@@ -6,8 +6,10 @@ module.exports = grammar({
 
     _block: ($) => choice($.paragraph),
 
+    paragraph: ($) => choice($._explicit_paragraph, $._implicit_paragraph),
+    _explicit_paragraph: ($) => seq('{p', repeat(choice($._none_eol_inline, $.hardbreak, $.softbreak)), 'p}'),
     // 连续的非空行就是 paragraph，后跟大于等于 1 个空行
-    paragraph: ($) => seq($._nonempty_line, repeat(seq($.softbreak, $._nonempty_line)), $.hardbreak),
+    _implicit_paragraph: ($) => seq($._nonempty_line, repeat(seq($.softbreak, $._nonempty_line)), $.hardbreak),
 
     _nonempty_line: ($) => repeat1($._none_eol_inline),
 
