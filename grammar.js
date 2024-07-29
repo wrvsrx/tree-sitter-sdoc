09a0cef314311519f44ef4767cffd032dae2dc76
wrvsrx <wrvsrx@outlook.com>
2024-07-29 15:30:01 +0800

    feat: support comment

--- grammar.js
+++ grammar.js
@@ -8,13 +8,14 @@ module.exports = grammar({
   rules: {
     document: ($) => repeat($._block),
 
-    _block: ($) => choice($.paragraph, $.section, $.heading, $.list),
+    _block: ($) => choice($.paragraph, $.section, $.heading, $.list, $.comment),
 
     paragraph: ($) => seq('{p', repeat($._inline), '}'),
     section: ($) => seq('{|', optional($.heading), repeat($._block), '}'),
     heading: ($) => seq('{#', repeat($._inline), '}'),
     list: ($) => seq('{l', repeat($.listitem), '}'),
     listitem: ($) => seq('{-', optional(choice(repeat($._block), repeat($._inline))), '}'),
+    comment: (_) => seq('{%', /([^{}]|\\\}|\\\{)+/ , '}'),
 
     _inline: ($) => choice($.word, $.softbreak),
 
