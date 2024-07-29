a9e1add3fd18a3a624e02039690df9f9a2afc2d7
wrvsrx <wrvsrx@outlook.com>
2024-07-29 14:13:05 +0800

    feat: support paragraph

--- grammar.js
+++ grammar.js
@@ -1,23 +1,16 @@
 module.exports = grammar({
-  name: "cfgm",
+  name: "sdoc",
 
   rules: {
     document: ($) => repeat($._block),
 
     _block: ($) => choice($.paragraph),
 
-    paragraph: ($) => choice($._explicit_paragraph, $._implicit_paragraph),
-    _explicit_paragraph: ($) => seq('{p', repeat(choice($._none_eol_inline, $.hardbreak, $.softbreak)), 'p}'),
-    // 连续的非空行就是 paragraph，后跟大于等于 1 个空行
-    _implicit_paragraph: ($) => seq($._nonempty_line, repeat(seq($.softbreak, $._nonempty_line)), $.hardbreak),
+    paragraph: ($) => seq('{p', repeat($._inline), '}'),
 
-    _nonempty_line: ($) => repeat1($._none_eol_inline),
+    _inline: ($) => choice($.word, $.softbreak),
 
-    _none_eol_inline: ($) => choice($.word),
-
-    // tokens
-    hardbreak: (_) => /\n\n+/,
-    softbreak: (_) => /\n/,
-    word: (_) => /\S+/,
+    word: (_) => /[^ {}]+/,
+    softbreak: (_) => '\n',
   },
 });
