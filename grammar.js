5df01febe55e974552ab3520bdb3d16d1bc1d275
wrvsrx <wrvsrx@outlook.com>
2024-07-29 14:49:28 +0800

    feat: support section and listing

--- grammar.js
+++ grammar.js
@@ -1,12 +1,20 @@
 module.exports = grammar({
   name: "sdoc",
 
+  conflicts: ($) => [
+    [$._block, $.section],
+  ],
+
   rules: {
     document: ($) => repeat($._block),
 
-    _block: ($) => choice($.paragraph),
+    _block: ($) => choice($.paragraph, $.section, $.heading, $.list),
 
     paragraph: ($) => seq('{p', repeat($._inline), '}'),
+    section: ($) => seq('{|', optional($.heading), repeat($._block), '}'),
+    heading: ($) => seq('{#', repeat($._inline), '}'),
+    list: ($) => seq('{l', repeat($.listitem), '}'),
+    listitem: ($) => seq('{-', optional(choice(repeat($._block), repeat($._inline))), '}'),
 
     _inline: ($) => choice($.word, $.softbreak),
 
