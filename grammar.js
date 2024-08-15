dcc03466d59ed38809965b10947883420f4dadba
wrvsrx <wrvsrx@outlook.com>
2024-08-15 10:07:50 +0800

    feat: add more queries

--- grammar.js
+++ grammar.js
@@ -8,30 +8,47 @@ module.exports = grammar({
   rules: {
     document: ($) => seq(optional($.meta), repeat($._block)),
 
-    meta: ($) => seq('{', token.immediate('meta'), repeat($.meta_block), '}'),
+    meta: ($) => seq('{', $.meta_marker, repeat($.meta_block), '}'),
     meta_block: ($) => seq('{', $.meta_name, choice(repeat($._inline), repeat($._block)), '}'),
 
-    _block: ($) => choice($.paragraph, $.section, $.heading, $.list, $.comment),
+    _block: ($) => choice(
+      $.paragraph,
+      $.section,
+      $.heading,
+      $.list,
+      $.comment,
+    ),
 
-    paragraph: ($) => seq('{p', repeat($._inline), '}'),
-    section: ($) => seq('{|', optional($.heading), repeat($._block), '}'),
+    paragraph: ($) => seq('{', $.paragraph_marker, repeat($._inline), '}'),
+    section: ($) => seq('{', $.section_marker, optional($.heading), repeat($._block), '}'),
     heading: ($) => seq('{', $.heading_marker, optional($.heading_content), '}'),
-    list: ($) => seq('{l', repeat($.listitem), '}'),
-    listitem: ($) => seq('{', $.list_marker, optional(choice(repeat($._block), repeat($._inline))), '}'),
+    list: ($) => seq('{', $.list_marker, repeat($.listitem), '}'),
+    listitem: ($) => seq('{', $.listitem_marker, optional(choice(repeat($._block), repeat($._inline))), '}'),
     comment: (_) => seq('{%', /([^{}]|\\\}|\\\{)+/, '}'),
-
     heading_content: ($) => repeat1($._inline),
 
-    _inline: ($) => choice($.word, $.softbreak, $.emphasis, $.strong),
+    _inline: ($) => choice(
+      $.word,
+      $.softbreak,
+      $.emphasis,
+      $.strong,
+      $.strikethrough,
+    ),
     emphasis: ($) => seq('{', $.emphasis_marker, repeat($._inline), '}'),
     strong: ($) => seq('{', $.strong_marker, repeat($._inline), '}'),
+    strikethrough: ($) => seq('{', $.strike_marker, repeat($._inline), '}'),
 
     word: (_) => /([^ {}]|\\\{|\\\})+/,
     meta_name: (_) => /([^ {}]|\\\{|\\\})+/,
     softbreak: (_) => '\n',
-    list_marker: (_) => token.immediate('-'),
+    section_marker: (_) => token.immediate('|'),
     heading_marker: (_) => token.immediate('#'),
+    paragraph_marker: (_) => token.immediate('p'),
+    list_marker: (_) => token.immediate('l'),
+    listitem_marker: (_) => token.immediate('-'),
     emphasis_marker: (_) => token.immediate('_'),
     strong_marker: (_) => token.immediate('*'),
+    strike_marker: (_) => token.immediate('~'),
+    meta_marker: (_) => token.immediate('meta'),
   },
 });
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -11,7 +11,12 @@
   "}"
 ] @punctuation.bracket
 (paragraph) @spell
+(listitem_marker) @markup.list
 (list_marker) @markup.list
 (emphasis) @markup.italic
 (strong) @markup.strong
+(strikethrough) @markup.strikethrough
+(meta_marker) @keyword.directive
 (meta_name) @variable.member
+(paragraph_marker) @type.builtin
+(list_marker) @type.builtin
